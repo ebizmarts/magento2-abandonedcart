3d850ce8aaa6c4cdca5fe6c334fe37d093abdcac
gonzalo <gonzalo@ebizmarts.com>
2015-07-31 16:15:57 -0300

    - modify email template - plugin for save the customer email in the quote in the checkout

---
+++ Model/Plugin/AccountManagement.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Author: info@ebizmarts.com
+ * Date: 7/31/15
+ * Time: 2:31 PM
+ * File: AccountManagement.php
+ * Module: magento2-abandonedcart
+ */
+namespace Ebizmarts\AbandonedCart\Model\Plugin;
+
+class AccountManagement
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $_logger;
+
+    /**
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->_objectManager = $objectManager;
+        $this->_logger = $logger;
+    }
+    public function aroundIsEmailAvailable(\Magento\Customer\Model\AccountManagement $accountManagement,\Closure $proceed,$customerEmail,$websiteId)
+    {
+        $ret = $proceed($customerEmail,$websiteId);
+        $session = $this->_getSession();
+        if($session)
+        {
+            $quoteId = $session->getQuoteId();
+            $quote = $this->_objectManager->create('\Magento\Quote\Model\Quote')->load($quoteId);
+            $quote->setCustomerEmail($customerEmail);
+            $quote->save();
+        }
+        return $ret;
+    }
+    protected function _getSession()
+    {
+        return $this->_objectManager->get('Magento\Checkout\Model\Session');
+    }
+}
